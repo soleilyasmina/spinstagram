0b4607975d8ca8bd7854ff37d8ad9e10d9b8ef78
soleilyasmina <mounlasolomon@gmail.com>
2021-02-19 11:29:58 -0500

    basically nada

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [caption, setCaption] = useState("");
+  const [author, setAuthor] = useState("");
+  const [image, setImage] = useState(null);
+
+  const handleSubmit = async (e) => {
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label htmlFor="caption">Caption:</label>
+        <input
+          type="text"
+          value={caption}
+          onChange={(e) => setCaption(e.target.value)}
+        />
+        <label htmlFor="author">Author:</label>
+        <input
+          type="text"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+        <label htmlFor="image">Image:</label>
+        <input
+          type="file"
+          value={image}
+          onChange={(e) => setImage(e.target.value)}
+        />
+        <button type="submit">Submit!</button>
+      </form>
     </div>
   );
 }
